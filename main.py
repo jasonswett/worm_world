1a0bfba118bd48f351e9230124e01c6a0d6d3a28
Jason Swett <jason@mhcare.com>
2022-01-22 12:50:39 -0800

    Misc.

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ def main():
     SCREEN_WIDTH = 40
     cell_screen = CellScreen(int(SCREEN_WIDTH * 1.5), SCREEN_WIDTH)
 
-    organism = Organism((20, 0))
+    organisms = [
+        Organism((20, 0))
+    ]
 
     while True:
         cell_screen.clear()
-        organism.advance()
-        cell_screen.draw_organism(organism)
+
+        for organism in organisms:
+            organism.advance()
+            cell_screen.draw_organism(organism)
+
         pygame.display.update()
-        time.sleep(0.2)
+        time.sleep(0.1)
 
 main()
