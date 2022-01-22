de009d0ed5e66ac979411500a1ee42f2707218a2
Jason Swett <jason@mhcare.com>
2022-01-22 16:42:45 -0500

    Increase worm size.

--- organism.py
+++ organism.py
@@ -11,7 +11,7 @@ class Organism:
     def __init__(self, position):
         self.x = position[0]
         self.y = position[1]
-        self.size = 16
+        self.size = 40
 
         self.cells = []
         starting_point = self.y
